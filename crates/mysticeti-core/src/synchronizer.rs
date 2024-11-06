dc6c628e197e2a5284c03e9e085839d6333f6fc1
Nikita Polianskii <nikita.polyansky@gmail.com>
2024-11-06 14:40:28 +0100

    lower sample precision

--- crates/mysticeti-core/src/synchronizer.rs
+++ crates/mysticeti-core/src/synchronizer.rs
@@ -36,7 +36,7 @@ impl Default for SynchronizerParameters {
             absolute_maximum_helpers: 10,
             maximum_helpers_per_authority: 2,
             batch_size: 10,
-            sample_precision: Duration::from_millis(500),
+            sample_precision: Duration::from_millis(300),
             grace_period: Duration::from_millis(150),
             stream_interval: Duration::from_secs(1),
             new_stream_threshold: 10,
