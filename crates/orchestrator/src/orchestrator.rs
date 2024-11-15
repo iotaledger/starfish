0835d63d1cfe1a42a1cfae4961200c7fc04438eb
ilyavorobev <vorobyev.i.v@yandex.ru>
2024-11-15 18:33:07 +0100

    undo last commit fix something about deploying

--- crates/orchestrator/src/orchestrator.rs
+++ crates/orchestrator/src/orchestrator.rs
@@ -223,8 +223,8 @@ impl<P: ProtocolCommands + ProtocolMetrics> Orchestrator<P> {
         // many ssh connections for too long.
         let commit = &self.settings.repository.commit;
         let command = [
-            "git fetch origin",
-            &format!("git checkout -B {commit} origin/{commit}"),
+            &format!("git fetch origin {commit}"),
+            &format!("(git checkout -b {commit} origin/{commit} || git checkout -f {commit})"),
             "source $HOME/.cargo/env",
             "RUSTFLAGS=-Ctarget-cpu=native cargo build --release",
         ]
