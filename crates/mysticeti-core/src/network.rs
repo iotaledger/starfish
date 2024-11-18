a0d9ec05a2890edcc49d6d8f2bc234f3c55855ae
Nikita Polianskii <nikita.polyansky@gmail.com>
2024-11-18 23:36:50 +0100

    tracing generated latency

--- crates/mysticeti-core/src/network.rs
+++ crates/mysticeti-core/src/network.rs
@@ -497,6 +497,7 @@ fn generate_latency(mean: f64) -> Duration {
 // Generate latency by adding the random deviation to the mean
     let latency = mean + rng.gen_range(-deviation..=deviation);
 
+    tracing::debug!("Generated latency {latency}");
 // Return the latency as a Duration (in milliseconds)
     Duration::from_millis(latency as u64)
 }
